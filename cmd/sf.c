97c73643da94869085bc7eaf20ee98db22b0c520
Eric Lee <eric.lee@embedian.com>
2024-07-27 02:11:49 +0000

    minor bugs fixed

--- cmd/sf.c
+++ cmd/sf.c
@@ -15,7 +15,7 @@
 #include <mapmem.h>
 #include <spi.h>
 #include <spi_flash.h>
-#include <asm/gpio.h>
+#include <asm/mach-imx/gpio.h>
 #include <asm/cache.h>
 #include <jffs2/jffs2.h>
 #include <linux/mtd/mtd.h>
